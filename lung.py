153210e43c1cf3869a69fc857ee457b06b16f45d
Salman Sayyed <75083770+salmansyyd@users.noreply.github.com>
2024-02-22 11:51:27 +0000

    updt: magic_model.py is ready!

--- lung.py
+++ lung.py
@@ -10,57 +10,127 @@ from sklearn.metrics import accuracy_score, classification_report
 ROOT_PATH = os.path.dirname(__file__)
 # Load the dataset
 
+
 def load_data():
     dataset = os.path.join(ROOT_PATH, "data", "lungcancer.csv")
     df = pd.read_csv(dataset)
-    df.drop(['index', 'Patient Id', 'Clubbing of Finger Nails'], axis=1, inplace=True)
+    df.drop(["index", "Patient Id", "Clubbing of Finger Nails"], axis=1, inplace=True)
     st.write(df.columns.tolist())
     return df
+
 
 df = load_data()
 
 # Map categorical variables to numerical representations
-gender_mapping = {'Male': 0, 'Female': 1}
-alcohol_use_mapping = {'Low': 0, 'Moderate': 1, 'High': 2}
-air_pollution_mapping = {'Low':0, 'Moderate':1, 'High':2}
-occupational_hazards_mapping = {'Low':0, 'Moderate':1, 'High':2}
-genetic_risk_mapping = {'Low':0, 'Moderate':1, 'High':2}
-chronic_lung_disease_mapping = {'None':0, 'Mild':1, 'Severe':2}
-dust_allergy_mapping = {'None': 0, 'Mild': 1, 'Severe': 2}
-balanced_diet_mapping = {'Poor':0 , 'Average':1 , 'Good':2}
-obesity_mapping = {'Not Obese':0, 'Obese':1}
-smoking_mapping = {'Non-Smoker':0, 'Ex-Smoker':1, 'Current Smoker':2}
-passive_smoker_mapping = {'Low':0, 'Moderate':1, 'High':2}
-chest_pain_mapping ={'Mild':0, 'High':1, 'Severe':2}
-coughing_of_blood_mapping = {'Mild':0, 'Moderate':1, 'Severe':2}
-fatigue_mapping = {'None':0, 'Mild':1, 'Severe':2}
-weight_loss_mapping = {'None':0, 'Mild':1, 'Severe':2}
-shortness_of_breath_mapping = {'None':0, 'Mild':1, 'Severe':2}
-wheezing_mapping = {'None':0, 'Mild':1, 'Severe':2}
-swallowing_difficulty_mapping = {'None':0, 'Mild':1, 'Severe':2}
-frequent_colds_mapping = {'None':0, 'Mild':1, 'Severe':2}
-dry_cough_mapping = {'None':0, 'Mild':1, 'Severe':2}
-snoring_mapping={'None':0, 'Mild':1, 'Severe':2}
+gender_mapping = {"Male": 0, "Female": 1}
+alcohol_use_mapping = {"Low": 0, "Moderate": 1, "High": 2}
+air_pollution_mapping = {"Low": 0, "Moderate": 1, "High": 2}
+occupational_hazards_mapping = {"Low": 0, "Moderate": 1, "High": 2}
+genetic_risk_mapping = {"Low": 0, "Moderate": 1, "High": 2}
+chronic_lung_disease_mapping = {"None": 0, "Mild": 1, "Severe": 2}
+dust_allergy_mapping = {"None": 0, "Mild": 1, "Severe": 2}
+balanced_diet_mapping = {"Poor": 0, "Average": 1, "Good": 2}
+obesity_mapping = {"Not Obese": 0, "Obese": 1}
+smoking_mapping = {"Non-Smoker": 0, "Ex-Smoker": 1, "Current Smoker": 2}
+passive_smoker_mapping = {"Low": 0, "Moderate": 1, "High": 2}
+chest_pain_mapping = {"Mild": 0, "High": 1, "Severe": 2}
+coughing_of_blood_mapping = {"Mild": 0, "Moderate": 1, "Severe": 2}
+fatigue_mapping = {"None": 0, "Mild": 1, "Severe": 2}
+weight_loss_mapping = {"None": 0, "Mild": 1, "Severe": 2}
+shortness_of_breath_mapping = {"None": 0, "Mild": 1, "Severe": 2}
+wheezing_mapping = {"None": 0, "Mild": 1, "Severe": 2}
+swallowing_difficulty_mapping = {"None": 0, "Mild": 1, "Severe": 2}
+frequent_colds_mapping = {"None": 0, "Mild": 1, "Severe": 2}
+dry_cough_mapping = {"None": 0, "Mild": 1, "Severe": 2}
+snoring_mapping = {"None": 0, "Mild": 1, "Severe": 2}
 
 
 # Sidebar for user inputs
 st.sidebar.title("Lung Cancer Prediction")
 st.sidebar.write("Please select your inputs:")
 
+
 def map_user_input(user_input, mapping_dict):
     return mapping_dict[user_input]
+
 
 # User inputs
 age = st.sidebar.slider("Age", min_value=0, max_value=100, value=50)
 
 # List of features
-features = ['Gender', 'Air Pollution', 'Alcohol Use', 'Occupational Hazards', 'Dust Allergy', 'Genetic Risk', 'Chronic Lung Disease', 'Balanced Diet', 'Obesity', 'Smoking', 'Passive Smoker', 'Chest Pain', 'Coughing of Blood', 'Fatigue', 'Weight Loss', 'Shortness of Breath', 'Wheezing', 'Swallowing Difficulty', 'Frequent Colds', 'Dry Cough', 'Snoring']
+features = [
+    "Gender",
+    "Air Pollution",
+    "Alcohol Use",
+    "Occupational Hazards",
+    "Dust Allergy",
+    "Genetic Risk",
+    "Chronic Lung Disease",
+    "Balanced Diet",
+    "Obesity",
+    "Smoking",
+    "Passive Smoker",
+    "Chest Pain",
+    "Coughing of Blood",
+    "Fatigue",
+    "Weight Loss",
+    "Shortness of Breath",
+    "Wheezing",
+    "Swallowing Difficulty",
+    "Frequent Colds",
+    "Dry Cough",
+    "Snoring",
+]
 
 # List of options for each feature
-options = [['Male', 'Female'], ['Low', 'Moderate', 'High'], ['Low', 'Moderate', 'High'], ['Low', 'Moderate', 'High'], ['None', 'Mild', 'Severe'], ['Low', 'Moderate', 'High'], ['None', 'Mild', 'Severe'], ['Poor', 'Average', 'Good'], ['Not Obese', 'Obese'], ['Non-Smoker', 'Ex-Smoker', 'Current Smoker'], ['Low', 'Moderate', 'High'], ['Mild', 'High', 'Severe'], ['Mild', 'Moderate', 'Severe'], ['None', 'Mild', 'Severe'], ['None', 'Mild', 'Severe'], ['None', 'Mild', 'Severe'], ['None', 'Mild', 'Severe'], ['None', 'Mild', 'Severe'], ['None', 'Mild', 'Severe'], ['None', 'Mild', 'Severe'], ['None', 'Mild', 'Severe']]
+options = [
+    ["Male", "Female"],
+    ["Low", "Moderate", "High"],
+    ["Low", "Moderate", "High"],
+    ["Low", "Moderate", "High"],
+    ["None", "Mild", "Severe"],
+    ["Low", "Moderate", "High"],
+    ["None", "Mild", "Severe"],
+    ["Poor", "Average", "Good"],
+    ["Not Obese", "Obese"],
+    ["Non-Smoker", "Ex-Smoker", "Current Smoker"],
+    ["Low", "Moderate", "High"],
+    ["Mild", "High", "Severe"],
+    ["Mild", "Moderate", "Severe"],
+    ["None", "Mild", "Severe"],
+    ["None", "Mild", "Severe"],
+    ["None", "Mild", "Severe"],
+    ["None", "Mild", "Severe"],
+    ["None", "Mild", "Severe"],
+    ["None", "Mild", "Severe"],
+    ["None", "Mild", "Severe"],
+    ["None", "Mild", "Severe"],
+]
 
 # List of mapping dictionaries
-mappings = [gender_mapping, air_pollution_mapping, alcohol_use_mapping, occupational_hazards_mapping, dust_allergy_mapping, genetic_risk_mapping, chronic_lung_disease_mapping, balanced_diet_mapping, obesity_mapping, smoking_mapping, passive_smoker_mapping, chest_pain_mapping, coughing_of_blood_mapping, fatigue_mapping, weight_loss_mapping, shortness_of_breath_mapping, wheezing_mapping, swallowing_difficulty_mapping, frequent_colds_mapping, dry_cough_mapping, snoring_mapping]
+mappings = [
+    gender_mapping,
+    air_pollution_mapping,
+    alcohol_use_mapping,
+    occupational_hazards_mapping,
+    dust_allergy_mapping,
+    genetic_risk_mapping,
+    chronic_lung_disease_mapping,
+    balanced_diet_mapping,
+    obesity_mapping,
+    smoking_mapping,
+    passive_smoker_mapping,
+    chest_pain_mapping,
+    coughing_of_blood_mapping,
+    fatigue_mapping,
+    weight_loss_mapping,
+    shortness_of_breath_mapping,
+    wheezing_mapping,
+    swallowing_difficulty_mapping,
+    frequent_colds_mapping,
+    dry_cough_mapping,
+    snoring_mapping,
+]
 
 # Dictionary to store numerical representations
 numerical_inputs = {}
@@ -71,21 +141,44 @@ for feature, option, mapping in zip(features, options, mappings):
     numerical_inputs[feature] = map_user_input(user_input, mapping)
 
 
-
 # Data preprocessing
 # Encode categorical variables
-df['Gender'] = df['Gender'].map({'Male': 0, 'Female': 1})
-df = pd.get_dummies(df, columns=[ 'Alcohol use', 'Dust Allergy', 'Balanced Diet','Air Pollution', 'OccuPational Hazards','Genetic Risk','Obesity', 'Smoking','chronic Lung Disease' ,
-                                  'Passive Smoker', 'Chest Pain', 'Coughing of Blood', 'Fatigue', 'Weight Loss', 
-                                  'Shortness of Breath', 'Wheezing', 'Swallowing Difficulty', 
-                                   'Frequent Cold', 'Dry Cough','Snoring'])
+
+df["Gender"] = df["Gender"].map({"Male": 0, "Female": 1})
+df = pd.get_dummies(
+    df,
+    columns=[
+        "Alcohol use",
+        "Dust Allergy",
+        "Balanced Diet",
+        "Air Pollution",
+        "OccuPational Hazards",
+        "Genetic Risk",
+        "Obesity",
+        "Smoking",
+        "chronic Lung Disease",
+        "Passive Smoker",
+        "Chest Pain",
+        "Coughing of Blood",
+        "Fatigue",
+        "Weight Loss",
+        "Shortness of Breath",
+        "Wheezing",
+        "Swallowing Difficulty",
+        "Frequent Cold",
+        "Dry Cough",
+        "Snoring",
+    ],
+)
 
 # Split data into features and target variable
-X = df.drop('Level', axis=1)
-y = df['Level']
+X = df.drop("Level", axis=1)
+y = df["Level"]
 
 # Train-test split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=98)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=98
+)
 
 # Model training
 rf_model = RandomForestClassifier(n_estimators=100, random_state=98)
@@ -95,19 +188,20 @@ rf_model.fit(X_train, y_train)
 feature_importances = rf_model.feature_importances_
 
 # Create a DataFrame to store feature importances
-feature_importance_df = pd.DataFrame({
-    'Feature': X_train.columns,
-    'Importance': feature_importances
-})
+feature_importance_df = pd.DataFrame(
+    {"Feature": X_train.columns, "Importance": feature_importances}
+)
 
 # Sort the DataFrame by importance in descending order
-feature_importance_df = feature_importance_df.sort_values(by='Importance', ascending=False)
+feature_importance_df = feature_importance_df.sort_values(
+    by="Importance", ascending=False
+)
 
 # Print feature importances
 st.write(feature_importance_df)
 
 # Select the top-k features (e.g., top 10 features)
-top_k_features = feature_importance_df.head(10)['Feature'].tolist()
+top_k_features = feature_importance_df.head(10)["Feature"].tolist()
 
 # Filter columns in the dataset to include only the top-k features
 X_train_selected = X_train[top_k_features]
@@ -117,7 +211,9 @@ X_test_selected = X_test[top_k_features]
 threshold = 0.01  # Example threshold value
 
 # Select features based on the threshold
-selected_features = feature_importance_df[feature_importance_df['Importance'] >= threshold]['Feature'].tolist()
+selected_features = feature_importance_df[
+    feature_importance_df["Importance"] >= threshold
+]["Feature"].tolist()
 
 # Filter columns in the dataset to include only the selected features
 X_train_selected = pd.DataFrame(X_train, columns=selected_features)
@@ -141,10 +237,12 @@ print(numerical_inputs)
 other_features = [0] * (X_train.shape[1] - len(user_input))
 input_data = np.array([user_input + other_features])
 
+
 # Define function to predict lung cancer
 def predict_lung_cancer(input_data):
     prediction = rf_model.predict(input_data)
     return prediction
+
 
 st.title("Model Performance")
 y_pred = rf_model.predict(X_test)
@@ -153,7 +251,7 @@ st.write("Accuracy:", accuracy)
 st.write("Classification Report:")
 st.write(classification_report(y_test, y_pred))
 
-if st.button('Predict'):
+if st.button("Predict"):
     prediction = predict_lung_cancer(input_data)
     st.title("Lung Cancer Prediction Result")
     st.write("Predicted Lung Cancer:", "Yes" if prediction[0] == 1 else "No")
--- magic_model.py
+++ magic_model.py
@@ -7,76 +7,206 @@ from sklearn.metrics import accuracy_score, classification_report
 
 ROOT_PATH = os.path.dirname(__file__)
 
+
 class MagicModel:
+    """
+    A class representing a magic model for predicting lung cancer.
+
+    Attributes:
+        df (pandas.DataFrame): The loaded dataset.
+        X_train (pandas.DataFrame): The training features.
+        X_test (pandas.DataFrame): The testing features.
+        y_train (pandas.Series): The training labels.
+        y_test (pandas.Series): The testing labels.
+        rf_model (RandomForestClassifier): The random forest model for feature selection.
+        rf_model_selected (RandomForestClassifier): The random forest model with selected features.
+
+    Methods:
+        load_data(): Loads the dataset and preprocesses it.
+        split_data(): Splits the data into training and testing sets.
+        train_model(): Trains the random forest model on the training data.
+        get_feature_importances(): Calculates the feature importances.
+        select_top_k_features(k): Selects the top k features based on importance.
+        select_features_based_on_threshold(threshold): Selects features based on a threshold.
+        select_features(features): Selects the specified features.
+        train_model_with_selected_features(X_train_selected): Trains the model with selected features.
+        evaluate_model_performance(X_test_selected): Evaluates the model performance on the testing data.
+        predict_lung_cancer(input_data): Predicts lung cancer based on input data.
+    """
+
     def __init__(self):
-        pass
+        self.df = self.load_data()
+        self.X_train, self.X_test, self.y_train, self.y_test = self.split_data()
+        self.rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
+        self.rf_model_selected = RandomForestClassifier(n_estimators=100, random_state=42)
+
+    def load_data(self):
+        """
+        Loads the dataset and preprocesses it.
+
+        Returns:
+            pandas.DataFrame: The preprocessed dataset.
+        """
+        dataset = os.path.join(ROOT_PATH, "data", "lungcancer.csv")
+        df = pd.read_csv(dataset)
+        df.drop(["index", "Patient Id", "Clubbing of Finger Nails"], axis=1, inplace=True)
+        df["Gender"] = df["Gender"].map({"Male": 0, "Female": 1})
+        df = pd.get_dummies(
+            df,
+            columns=[
+                "Alcohol use",
+                "Dust Allergy",
+                "Balanced Diet",
+                "Air Pollution",
+                "OccuPational Hazards",
+                "Genetic Risk",
+                "Obesity",
+                "Smoking",
+                "chronic Lung Disease",
+                "Passive Smoker",
+                "Chest Pain",
+                "Coughing of Blood",
+                "Fatigue",
+                "Weight Loss",
+                "Shortness of Breath",
+                "Wheezing",
+                "Swallowing Difficulty",
+                "Frequent Cold",
+                "Dry Cough",
+                "Snoring",
+            ],
+        )
+        return df
+
+    def split_data(self):
+        """
+        Splits the data into training and testing sets.
+
+        Returns:
+            tuple: A tuple containing X_train, X_test, y_train, y_test.
+        """
+        X = self.df.drop("Level", axis=1)
+        y = self.df["Level"]
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2, random_state=42
+        )
+        return X_train, X_test, y_train, y_test
+
+    def train_model(self):
+        """
+        Trains the random forest model on the training data.
+        """
+        self.rf_model.fit(self.X_train, self.y_train)
+
+    def get_feature_importances(self):
+        """
+        Calculates the feature importances.
+
+        Returns:
+            pandas.DataFrame: A DataFrame containing the feature importances.
+        """
+        feature_importances = self.rf_model.feature_importances_
+        feature_importance_df = pd.DataFrame(
+            {"Feature": self.X_train.columns, "Importance": feature_importances}
+        )
+        feature_importance_df = feature_importance_df.sort_values(
+            by="Importance", ascending=False
+        )
+        return feature_importance_df
+
+    def select_top_k_features(self, k):
+        """
+        Selects the top k features based on importance.
+
+        Args:
+            k (int): The number of top features to select.
+
+        Returns:
+            list: A list of the top k features.
+        """
+        feature_importance_df = self.get_feature_importances()
+        top_k_features = feature_importance_df.head(k)["Feature"].tolist()
+        return top_k_features
+
+    def select_features_based_on_threshold(self, threshold):
+        """
+        Selects features based on a threshold.
+
+        Args:
+            threshold (float): The importance threshold.
+
+        Returns:
+            list: A list of selected features.
+        """
+        feature_importance_df = self.get_feature_importances()
+        selected_features = feature_importance_df[
+            feature_importance_df["Importance"] >= threshold
+        ]["Feature"].tolist()
+        return selected_features
+
+    def select_features(self, features):
+        """
+        Selects the specified features.
+
+        Args:
+            features (list): A list of features to select.
+
+        Returns:
+            tuple: A tuple containing the selected training and testing features.
+        """
+        X_train_selected = pd.DataFrame(self.X_train, columns=features)
+        X_test_selected = pd.DataFrame(self.X_test, columns=features)
+        return X_train_selected, X_test_selected
+
+    def train_model_with_selected_features(self, X_train_selected):
+        """
+        Trains the model with selected features.
+
+        Args:
+            X_train_selected (pandas.DataFrame): The selected training features.
+        """
+        self.rf_model_selected.fit(X_train_selected, self.y_train)
+
+    def evaluate_model_performance(self, X_test_selected):
+        """
+        Evaluates the model performance on the testing data.
+
+        Args:
+            X_test_selected (pandas.DataFrame): The selected testing features.
+
+        Returns:
+            float: The accuracy of the model.
+        """
+        y_pred_selected = self.rf_model_selected.predict(X_test_selected)
+        accuracy_selected = accuracy_score(self.y_test, y_pred_selected)
+        return accuracy_selected
+
+    def predict_lung_cancer(self, input_data):
+        """
+        Predicts lung cancer based on input data.
+
+        Args:
+            input_data (array-like): The input data for prediction.
+
+        Returns:
+            array-like: The predicted lung cancer values.
+        """
+        prediction = self.rf_model.predict(input_data)
+        return prediction
 
 
-def load_data(self):
-    dataset = os.path.join(ROOT_PATH, "data", "lungcancer.csv")
-    df = pd.read_csv(dataset)
-    df.drop(['index', 'Patient Id', 'Clubbing of Finger Nails'], axis=1, inplace=True)
-    return df
+if __name__ == "__main__":
+    model = MagicModel()
+    model.train_model()
+    top_k_features = model.select_top_k_features(10)
+    X_train_selected, X_test_selected = model.select_features(top_k_features)
+    model.train_model_with_selected_features(X_train_selected)
+    accuracy_selected = model.evaluate_model_performance(X_test_selected)
 
+    print("Accuracy with selected features:", accuracy_selected)
+    user_input = [50, 2, 2, 0, 2, 2, 0, 0, 1, 0, 0, 2, 0, 0, 1, 0, 2, 0, 2, 1, 2]
+    other_features = [0] * (model.X_train.shape[1] - len(user_input))
 
-df = load_data()
-
-df['Gender'] = df['Gender'].map({'Male': 0, 'Female': 1})
-df = pd.get_dummies(df, columns=[ 'Alcohol use', 'Dust Allergy', 'Balanced Diet','Air Pollution', 'OccuPational Hazards','Genetic Risk','Obesity', 'Smoking','chronic Lung Disease' ,
-                                  'Passive Smoker', 'Chest Pain', 'Coughing of Blood', 'Fatigue', 'Weight Loss', 
-                                  'Shortness of Breath', 'Wheezing', 'Swallowing Difficulty', 
-                                   'Frequent Cold', 'Dry Cough','Snoring'])
-
-# Split data into features and target variable
-X = df.drop('Level', axis=1)
-y = df['Level']
-
-# Train-test split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Model training
-rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
-rf_model.fit(X_train, y_train)
-
-# Get feature importances
-feature_importances = rf_model.feature_importances_
-
-# Create a DataFrame to store feature importances
-feature_importance_df = pd.DataFrame({
-    'Feature': X_train.columns,
-    'Importance': feature_importances
-})
-
-# Sort the DataFrame by importance in descending order
-feature_importance_df = feature_importance_df.sort_values(by='Importance', ascending=False)
-
-# Select the top-k features (e.g., top 10 features)
-top_k_features = feature_importance_df.head(10)['Feature'].tolist()
-
-# Filter columns in the dataset to include only the top-k features
-X_train_selected = X_train[top_k_features]
-X_test_selected = X_test[top_k_features]
-
-# Choose a threshold or determine the number of top features to select
-threshold = 0.01  # Example threshold value
-
-# Select features based on the threshold
-selected_features = feature_importance_df[feature_importance_df['Importance'] >= threshold]['Feature'].tolist()
-
-# Filter columns in the dataset to include only the selected features
-X_train_selected = pd.DataFrame(X_train, columns=selected_features)
-X_test_selected = pd.DataFrame(X_test, columns=selected_features)
-
-# Train the Random Forest classifier with the selected features
-rf_model_selected = RandomForestClassifier(n_estimators=100, random_state=42)
-rf_model_selected.fit(X_train_selected, y_train)
-
-# Evaluate model performance
-y_pred_selected = rf_model_selected.predict(X_test_selected)
-accuracy_selected = accuracy_score(y_test, y_pred_selected)
-print("Accuracy with selected features:", accuracy_selected)
-other_features = [0] * (X_train.shape[1] - len(user_input))
-
-def predict_lung_cancer(input_data):
-    prediction = rf_model.predict(input_data)
-    return prediction
+    model_input = np.array([user_input + other_features])
+    prediction = model.predict_lung_cancer(model_input)
+    print("Prediction:", prediction)
